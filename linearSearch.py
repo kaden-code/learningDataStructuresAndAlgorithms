ccc11b7a9b872076f9d7d93a6e43af58288f4a0e
kaden-code <118853686+kaden-code@users.noreply.github.com>
2024-01-07 15:11:38 -0500

    altered linear search function and added function to verify search

--- linearSearch.py
+++ linearSearch.py
@@ -1,8 +1,19 @@
 def linearSearch(list,target):
     """ returns value of the target if found, if not returns error"""
-    for i in list:
-        if list[i] == target:
-            print("Target value found")
-            return i
+    for i in range(0,len(list)):
+      if list[i] == target:
+          return i
     return None
 
+
+def verify(index):
+    if index != None:
+      print("Target value index:" , index)
+    else:
+      print("Target not found")
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+
+result = linearSearch(numbers,8)
+verify(result)
