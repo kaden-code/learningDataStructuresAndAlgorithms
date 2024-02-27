53dc4465d44abfa5cb156bd93e27022dc77867d2
kaden-code <118853686+kaden-code@users.noreply.github.com>
2024-02-27 09:15:58 -0500

    Creates linked list class

--- linkedList.py
+++ linkedList.py
@@ -13,3 +13,22 @@ class Node:
         return "<Node data: %s>" %self.data
     
 
+class linkedList:
+    """
+    Singly linked list
+    """
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+    
+    def size(self):
+        current = self.head
+        count = 0
+
+        while current != None:
+            count += 1
+            current = current.nextNode
+        return count
+
